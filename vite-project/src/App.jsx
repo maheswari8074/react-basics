649efb2fedd1b764bc7c0f6a64c4097f523f7f5f
Maheswari8074 <maheswarimudadla756@gmail.com>
2025-05-25 13:19:22 +0530

    02change

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,12 @@
 import Mini from "./mini";
 function App() {
- 
+ const username = "Maheswari"
  return(
-  // <h1>Hello World!!Let's Start Learning🚀</h1>
+  <>
+  <h1>Hello World!!Let's Start Learning🚀{username}</h1>
    <Mini/>
+  </>
+  
  );
 }
 export default App;
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,45 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import React from 'react';
+
 
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function MyApp(){
+    return (
+        <div>
+            <h1>Custom App | APP</h1>
+        </div>
+    )
+}
+
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// } 
+// ->cant write react elements like this in react (raw objects)
+
+const anotherElement = (
+    <a href="https://google.com" target='_blank'>✌️Visit google✌️</a>
+);
+
+const anotherUser ="Bhuvana"
+
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://google.com',target: '_blank' },
+    'click me to visit google',
+    //this is called as tree after this we cann add varbles
+    anotherElement
 )
+createRoot(document.getElementById('root')).render(
+ 
+   
+
+   reactElement
+  
+);
