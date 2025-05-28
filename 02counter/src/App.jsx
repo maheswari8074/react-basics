4145130867706742bfc97ce0a4ca0ee3d24130ce
Maheswari8074 <maheswarimudadla756@gmail.com>
2025-05-28 17:23:18 +0530

    updt counter functn

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,42 +1,44 @@
-import { useState } from 'react'//hook comes frm here
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"; //hook comes frm here
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
   //useState  responsble for changes in the state(value updt) in dom
-let [counter,setCounter]=useState(15)
-// [var,method_controlsvar]
+  let [counter, setCounter] = useState(15);
+  // [var,method_controlsvar]
   // let counter = 15
 
-  const addValue = () =>{
+  const addValue = () => {
+    console.log("clicked", counter);
+    counter = counter + 1;
+    setCounter(counter);
+    setCounter(counter);
+    setCounter(counter);
+    setCounter(counter);
+  };
 
-   
-       console.log("clicked",counter);
-        counter= counter+1
-       setCounter(counter)
-  }
-
-  const removeValue = () =>{
-    if(counter<=0)counter=0;
-    else{
-    setCounter(counter-1);
+  const removeValue = () => {
+    if (counter <= 0) return 0;
+    else {
+      setCounter(prevCounter =>Math.max(prevCounter-1,0));
+      setCounter(prevCounter =>Math.max(prevCounter-1,0));
+      setCounter(prevCounter =>Math.max(prevCounter-1,0));
     }
-  }
+  };
   return (
     <>
-      
-     <h1>Counter project -learn</h1>
-     <h2>Counter value: {counter}</h2>
+      <h1>Counter project -learn</h1>
+      <h2>Counter value: {counter}</h2>
 
-     <button onClick ={addValue}>Add value{counter}</button>
-     <br />
-     <button onClick={removeValue}>remove value{counter}</button>
-     <p>footer:{counter}</p>
+      <button onClick={addValue}>Add value{counter}</button>
+      <br />
+      <button onClick={removeValue}>remove value{counter}</button>
+      <p>footer:{counter}</p>
     </>
-  )
+  );
 }
 
 //to update vars -in UI React controls it
 
-export default App
+export default App;
